4a69a5b598b5678b986fe141bff02f03f3eab0e3
yeojin12 <kyj713102@naver.com>
2025-04-30 23:08:44 +0900

    6주차-1번

--- 6주차-1번.py
+++ 6주차-1번.py
@@ -1,6 +1,31 @@
+class Student:
+    def __init__(self,name,score):
+        self.name = name
+        self.score = score
+
+    def get_grade(self):
+        if self.score >= 90:
+            return "A" 
+        elif self.score >= 80:
+            return "B"
+        elif self.score >= 70:
+            return "C"
+        else:
+            return "F"
+        
+    def print_list1(self):
+        print(f"{student.name} : {student.get_grade()}")
+        
 N = int(input())
-name = input().split()
+list1 = []
 
-list1 = sorted(name, key=lambda x: (len(x), x))
+for i in range(1, N+1):
+    name, score = input().split()
+    score = int(score)
+    student = Student(name, score)
+    list1.append(student)
 
-print(" ".join(list1))
+print()
+
+for student in list1:
+    student.print_list1()
